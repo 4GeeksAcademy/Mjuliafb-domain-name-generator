4c7ba8eebb4b9e77b4c6f016383014c0fabaa215
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-11-28 23:26:39 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+const anyString = () => {
+  let string = "";
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for (let key in pronoun);
+  console.log(pronoun[i])
+
+}
+  
+
   console.log("Hello Rigo from the console!");
 };
---
+++ src/app2.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    //write your code here
+  const anyString = () => {
+    let string = "";
+  
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+  
+    for (let key in pronoun);
+    console.log(pronoun[i])
+  
+  }
