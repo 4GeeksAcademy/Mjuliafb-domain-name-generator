921cea8b74ad7f9a2e8155c676d081da3d79273d
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-11-29 00:33:57 +0000

    fifth commit hacks and nested loop fix

--- src/app2.js
+++ src/app2.js
@@ -2,26 +2,32 @@ window.onload = function() {
     //write your code here
     const anyString = () => {
 
-      let pronoun = ['the', 'our'];
-      let adj = ['great', 'big'];
-      let noun = ['jogger', 'racoon'];
-      let dot = ['.com', '.net'];
-  
-      let pronounIndex = Math.floor(Math.random() * pronoun.length);
-      let adjIndex = Math.floor(Math.random() * adj.length);
-      let nounIndex = Math.floor(Math.random() * noun.length);
-      let dotIndex = Math.floor(Math.random() * dot.length);
-  
-      for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
-          for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
-              for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
-                  for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
-                      console.log(pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + dot[dotIndex]);
-                  }
-              }
-          }
-      }
-  }
-  
-  // Llamada a la función
-  anyString();
+        let pronoun = ['las', 'los','nuestros' ];
+        let adj = ['grandes', 'aflables','amable'];
+        let noun = ['animal', 'conejos','miop'];
+        let dot = ['.com', '.net', ".org"];
+    
+        let pronounIndex = Math.floor(Math.random() * pronoun.length);
+        let adjIndex = Math.floor(Math.random() * adj.length);
+        let nounIndex = Math.floor(Math.random() * noun.length);
+        let dotIndex = Math.floor(Math.random() * dot.length);
+        
+            for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+                for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+                    for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+                        for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
+                            // Tu condición if aquí
+                            if (noun[nounIndex] === 'animal'|| noun[nounIndex] === 'miop' ) {
+                                console.log(pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ".es");
+                            } else {
+                                // Console.log solo si la condición no se cumple
+                                console.log(pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + dot[dotIndex]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        // Llamada a la función
+        anyString();
