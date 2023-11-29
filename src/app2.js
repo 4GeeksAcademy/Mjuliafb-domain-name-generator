4cef2828cc662004137a125cdd789f1735fe2f29
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-11-29 17:45:20 +0000

    review issues3

--- src/app2.js
+++ src/app2.js
@@ -5,7 +5,7 @@ window.onload = function() {
         let pronoun = ['las', 'los','nuestros' ];
         let adj = ['grandes', 'aflables','amables'];
         let noun = ['animal', 'conejos','miop'];
-        let dot = ['.com', '.net', ".org"];
+        let dot = ['.com', '.net', ".org", ".es"];
     
        //  let pronounIndex = Math.floor(Math.random() * pronoun.length);
       //  let adjIndex = Math.floor(Math.random() * adj.length);
@@ -15,19 +15,12 @@ window.onload = function() {
       for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
         for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
             for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
-                for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
-                    // Tu condición if aquí
-                    if (noun[nounIndex] === 'animal'|| noun[nounIndex] === 'miop' ) {
-                        console.log(`${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}` + ".es");
-                    } else {
-                        // Console.log solo si la condición no se cumple
-                        console.log(`${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}${dot[dotIndex]}`);
-                    }
+                for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {                
+                    console.log(`${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}${dot[dotIndex]}`);
                 }
             }
         }
     }
-}
         
         // Llamada a la función
         anyString();
