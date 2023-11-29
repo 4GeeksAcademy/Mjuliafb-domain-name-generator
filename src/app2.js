5346880366a35bc5d48e8a6cf46c68339f26c8b2
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-11-29 17:41:58 +0000

    review issues2

--- src/app2.js
+++ src/app2.js
@@ -12,22 +12,22 @@ window.onload = function() {
       //  let nounIndex = Math.floor(Math.random() * noun.length);
       //  let dotIndex = Math.floor(Math.random() * dot.length);
         
-            for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
-                for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
-                    for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
-                        for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
-                            // Tu condición if aquí
-                            if (noun[nounIndex] === 'animal'|| noun[nounIndex] === 'miop' ) {
-                                console.log(pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ".es");
-                            } else {
-                                // Console.log solo si la condición no se cumple
-                                console.log(pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + dot[dotIndex]);
-                            }
-                        }
+      for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+        for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+            for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+                for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
+                    // Tu condición if aquí
+                    if (noun[nounIndex] === 'animal'|| noun[nounIndex] === 'miop' ) {
+                        console.log(`${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}` + ".es");
+                    } else {
+                        // Console.log solo si la condición no se cumple
+                        console.log(`${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}${dot[dotIndex]}`);
                     }
                 }
             }
         }
+    }
+}
         
         // Llamada a la función
         anyString();
