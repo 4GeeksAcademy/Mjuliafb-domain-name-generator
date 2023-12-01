b522d8b464d2b99fcea9be753f266762c0877919
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-12-01 19:27:16 +0000

    apps.js only reviewed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const generateString = (pronoun, adj, noun, dot) => {
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        for (let dotIndex = 0; dotIndex < dot.length; dotIndex++) {
+          console.log(
+            `${pronoun[pronounIndex]}${adj[adjIndex]}${noun[nounIndex]}${dot[dotIndex]}`
+          );
+        }
+      }
+    }
+  }
+};
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["las", "los", "nuestros"];
+  const adj = ["grandes", "aflables", "amables"];
+  const noun = ["animal", "conejos", "miop"];
+  const dot = [".com", ".net", ".org", ".es"];
+
+  generateString(pronoun, adj, noun, dot);
 };
